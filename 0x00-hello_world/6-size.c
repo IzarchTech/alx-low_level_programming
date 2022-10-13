4024e03acd00fd6a8c87397249415e94722306c2
Chukwunwike Udezue <izarchtechnologies@hotmail.com>
2022-10-13 15:25:44 -0700

    6-size C script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long long my_int;
+	long my_long_int;
+	float f;
+
+	printf("Size of a char: %d bytes(s)\n", sizeof(c));
+	printf("Size of an int: %d bytes(s)\n", sizeof(i));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(my_long_int));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(my_int));
+	printf("Size of a float: %d bytes(s)\n", sizeof(c));
+	return (0);
+}
