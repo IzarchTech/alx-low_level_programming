e9c10821ca560a7102094cb69a0456824248b858
Chukwunwike Udezue <izarchtechnologies@hotmail.com>
2022-10-14 16:17:44 -0700

    4-print-alphabt Script

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x == 'e' || x == 'q')
+			continue;
+		putchar(x);
+		x++;
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
