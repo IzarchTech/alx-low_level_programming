2f90bb0b19fb0fc27eb7fc13b3e996241dd511e3
Chukwunwike Udezue <izarchtechnologies@hotmail.com>
2022-10-18 16:24:01 -0700

    2-print_alphabet_x10 script

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lower case alphabets 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j;
+	for(; i < 10; i++)
+	{
+		for (j = 'a'; i <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
