dfede37edd2578b7582ad200401b47a0ea58c80c
Chukwunwike Udezue <izarchtechnologies@hotmail.com>
2022-10-18 17:09:11 -0700

    3-islower.c script

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ * @c: is the letter being tested
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 96 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
