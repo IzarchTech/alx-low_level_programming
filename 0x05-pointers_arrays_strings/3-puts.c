a524a2ee03bb3515970dd905afa4d2d34678bf85
Chukwunwike Udezue <izarchtechnologies@hotmail.com>
2022-10-24 15:58:33 -0700

    3 puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print to screen
+ * @str: a string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
