a78c6c945fee37c20f9bd03c5764323424d960ee
Chukwunwike Udezue <izarchtechnologies@hotmail.com>
2022-10-26 13:51:00 -0700

    _strcat script

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ *
+ * Return: the pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	for (x = 0; dest[x] != '\0'; x++)
+		x = x;
+
+	for (y = 0; src[y] != '\0'; y++)
+	{
+		dest[x] = src[y];
+		x++;
+	}
+
+	dest[x] = '\0';
+	return (dest);
+}
