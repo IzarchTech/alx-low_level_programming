b559549035c7be5a2e44512fefeea4f56d84e286
Chukwunwike Udezue <izarchtechnologies@hotmail.com>
2022-11-02 14:38:54 -0700

    _puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
-* _puts_recursion - puts function using recursion
-* @s: string
-*/
+ * _puts_recursion - The Recursion for String.
+ *
+ * @s: string
+ *
+ * return: - 0.
+ */
+
 void _puts_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else
-    {
-        _putchar('\n');
-    }
+	if (*s != '\0')
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar ('\n');
 }
