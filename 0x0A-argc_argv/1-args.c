90291aa4ec8e70c8581a1e75e71a9392ac9dfa3c
Chukwunwike Udezue <izarchtechnologies@hotmail.com>
2022-11-04 15:42:42 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - app entry
+ * @argc: argument size
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	exit(EXIT_SUCCESS);
+}
