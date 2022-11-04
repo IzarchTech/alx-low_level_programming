01a596721899e6a04dd258683beef500dc45787d
Chukwunwike Udezue <izarchtechnologies@hotmail.com>
2022-11-04 15:27:10 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - app entry
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	printf("\n");
+	exit(EXIT_SUCCESS);
+}
